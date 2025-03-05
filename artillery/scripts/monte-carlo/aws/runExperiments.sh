e18226b9fc6856bf54b751bb2d0ff6fede0eebfd
IlPontefice <micheleoliva97@gmail.com>
2025-03-05 21:04:39 +0100

    Updating the Tool - 4

--- artillery/scripts/monte-carlo/aws/runExperiments.sh
+++
@@ -1,6 +0,0 @@
-artillery run cold.yaml -t "https://mg29etspsa.execute-api.eu-central-1.amazonaws.com/dev/lambda"
-./script.sh 1000000
-artillery run cold.yaml -t "https://mg29etspsa.execute-api.eu-central-1.amazonaws.com/dev/lambda"
-./script.sh 1050000
-artillery run cold.yaml -t "https://mg29etspsa.execute-api.eu-central-1.amazonaws.com/dev/lambda"
-./script.sh 1102500
