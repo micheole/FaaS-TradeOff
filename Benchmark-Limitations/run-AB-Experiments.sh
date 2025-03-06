1381af4cdb9bce67d7b541ae3462b588ffd49980
IlPontefice <micheleoliva97@gmail.com>
2025-03-06 10:39:26 +0100

    Updating the Tool - 8

--- Benchmark-Limitations/run-AB-Experiments.sh
+++ Benchmark-Limitations/run-AB-Experiments.sh
@@ -1,4 +1,14 @@
 ./orchestrator.sh call-limited 5000000 5250000
-# ./orchestrator.sh time-limited 5000000 5250000
-# ./orchestrator.sh accuracy-limited 5000000 5250000
-# ./orchestrator.sh money-limited 5000000 5250000
+./orchestrator.sh time-limited 5000000 5250000
+./orchestrator.sh accuracy-limited 5000000 5250000
+./orchestrator.sh money-limited 5000000 5250000
+
+./orchestrator.sh call-limited 5000000 5500000
+./orchestrator.sh time-limited 5000000 5500000
+./orchestrator.sh accuracy-limited 5000000 5500000
+./orchestrator.sh money-limited 5000000 5500000
+
+./orchestrator.sh call-limited 5000000 5750000
+./orchestrator.sh time-limited 5000000 5750000
+./orchestrator.sh accuracy-limited 5000000 5750000
+./orchestrator.sh money-limited 5000000 5750000
